f0552b97f7f8ee23316886f417b4552b38c5a40e
govind parmar <164615630+govind-parmar3@users.noreply.github.com>
2024-12-03 00:39:58 +0530

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 
 @app.route('/api/scrape', methods=['GET'])
 def scrape():
-    keyword = request.args.get('keyword', 'developer')  # URL से keyword ले रहे हैं
-    with ThreadPoolExecutor() as executor:  # Multi-threading
+    keyword = request.args.get('keyword', 'developer')  
+    with ThreadPoolExecutor() as executor: 
         future = executor.submit(scrape_jobs, keyword)
         jobs = future.result()
-    return jsonify(jobs)  # JSON डेटा return करें
+    return jsonify(jobs) 
 
 if __name__ == '__main__':
-    app.run(debug=True)  # Server run करें
+    app.run(debug=True) 
